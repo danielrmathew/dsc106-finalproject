a6ae6d5b3879c836185bd3810b4912f6073ce2e5
Rohan Duvur <rohan@Sreerams-MBP-10.lan>
2024-02-28 18:07:18 -0800

    polynomial checking

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -5,17 +5,83 @@
 
   
   let poly;
-  function validatePoly() {
-    var p = new Polynomial(poly);
-    let validPoly = true;
-    console.log(p);
-    let arr = Object.keys(p.coeff);
-    for (let i = 0; i < arr.length; i++) {
-      console.log(arr[i]);
-      if (isNaN(p.coeff[arr[i]])) {
-        console.log('shitass poly')
-        validPoly = false;
+
+  function checkValidParentheses(str) {
+    const stack = [];
+    const map = {
+      '(': ')'
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '(') {
+        stack.push(str[i]);
+      } else {
+        let last = stack.pop();
+        if (str[i] !== map[last]) {
+          return false;
+        }
       }
+    }
+
+    if (stack.length !== 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  function isValidPolynomial(str) {
+    // Remove whitespace from the string
+    str = str.replace(/\s/g, '');
+
+    // Make sure there are no back to back operators
+    const noBackToBackOperators = str.match(/(\+|\-|\*\/){2,}/);
+    if (noBackToBackOperators) {
+      console.log('I failed at back to back operators');
+      return false;
+    }
+
+    // if the term ends with an operator, it is invalid
+    const endsWithOperator = str.match(/(\+|\-|\*\/)$/);
+    if (endsWithOperator) {
+      console.log('I failed at ends with operator');
+      return false;
+    }
+
+    const terms = str.match(/(\+|\-|\*\/)?[a-z0-9.^]+/gi);
+
+    console.log(terms);
+
+    for (let i = 0; i < terms.length; i++) {
+      const term = terms[i];
+      // make sure term only contains valid characters
+      const validTerm = term.match(/^(\+|\-|\*\/)?[0-9x.]+(\^)?[0-9x.]*?$/i);
+
+      if (!validTerm) {
+        console.log('I failed at valid term');
+        return false;
+      }
+
+      // if there are parentheses, make sure they are valid
+      if (term.includes('(') || term.includes(')')) {
+        const validParentheses = checkValidParentheses(term);
+        if (!validParentheses) {
+          console.log('I failed at valid parentheses');
+          return false;
+        }
+      }
+    }
+
+    return true;
+}
+
+  function validatePoly() {
+    if (isValidPolynomial(poly)) {
+      console.log('Valid polynomial');
+      var p = new Polynomial(poly);
+      console.log(p);
+    } else {
+      console.log('Invalid polynomial');
     }
 
     // if (validPoly) {}
